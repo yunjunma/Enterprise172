99d8405fc921a27ca8796cc6a788ba96e73bed5e
Yunjun Ma <yunjunma@gmail.com>
2018-11-08 13:57:57 -0800

    table added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'employee'
+});
+
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
