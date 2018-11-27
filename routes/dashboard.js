0a818c76c47024a02c1fe3d9eed6dc83bdc4f370
Tom Luong <tomvna@yahoo.com>
2018-11-27 15:42:53 -0800

    Finish dashboard display

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,12 +8,18 @@ var credentials = {connectionLimit: 10};
 
 router.get('/', function (req, res) {
     var pool = mysql.createPool(credentials);
-    var query1 = "SELECT COUNT(DISTINCT title) from titles";
-    var query2 = "SELECT COUNT(emp_no) FROM employees";
-    var query3 = "SELECT SUM(salary) FROM salaries";
-    var query4 = "SELECT COUNT(dept_name) FROM departments";
-    var query5 = "SELECT COUNT(dept_name) FROM departments";
-    var query6 = "SELECT title, MIN(age), MAX(age) FROM User, Loan where User.uID = Loan.uID group by title;";
+    var query1 = "SELECT COUNT(DISTINCT title) as title from titles";
+    var query2 = "SELECT COUNT(emp_no) as employee FROM employees";
+    var query3 = "SELECT SUM(salary) as money FROM salaries";
+    var query4 = "SELECT COUNT(dept_name) as department FROM departments";
+    var query5 = "SELECT COUNT(emp_no) as employee FROM employees where YEAR(hire_date) = 2014";
+    var query6 = "SELECT COUNT(emp_no) as employee FROM employees where YEAR(hire_date) = 2015";
+    var query7 = "SELECT COUNT(emp_no) as employee FROM employees where YEAR(hire_date) = 2016";
+    var query8 = "SELECT COUNT(emp_no) as employee FROM employees where YEAR(hire_date) = 2017";
+    var query9 = "SELECT COUNT(emp_no) as employee FROM employees where YEAR(hire_date) = 2018";
+    var query10 = "SELECT title, MIN(age), MAX(age) FROM User, Loan where User.uID = Loan.uID group by title;"; // Not sure which query this is for so I will just leave this as is
+    
+    //var query10 = "SELECT YEAR(hire_date) as year FROM employees";
 
     var return_data = {};
 /*
@@ -39,14 +45,23 @@ router.get('/', function (req, res) {
         function(callback) { connection.query(query4, callback) },
         function(callback) { connection.query(query5, callback) },
         function(callback) { connection.query(query6, callback) },
+        function(callback) { connection.query(query7, callback) },
+        function(callback) { connection.query(query8, callback) },
+        function(callback) { connection.query(query9, callback) },
+        //function(callback) { connection.query(query10, callback) },
       ], function(err, results) {
-        console.log(JSON.stringify(results[0][0]))
-        res.render('dashboard', { rows : (JSON.stringify(results[0][0]))["COUNT(DISTINCT title)"], 
-                                rows2 : JSON.stringify(results[1][0]).COUNT(emp_no), 
-                                rows3 : JSON.stringify(results[2][0]), 
-                                rows4 : JSON.stringify(results[3][0]), 
-                                rows5 : JSON.stringify(results[4][0]), 
-                                rows6 : JSON.stringify(results[5][0]) 
+        //console.log(JSON.stringify(results[9][0][0]["year"]));
+        res.render('dashboard', { rows : JSON.stringify(results[0][0][0]["title"]), 
+                                rows2 : JSON.stringify(results[1][0][0]["employee"]), 
+                                rows3 : JSON.stringify(results[2][0][0]["money"]), 
+                                rows4 : JSON.stringify(results[3][0][0]["department"]), 
+                                rows5 : JSON.stringify(results[4][0][0]["employee"]), 
+                                rows6 : JSON.stringify(results[5][0][0]["employee"]), 
+                                rows7 : JSON.stringify(results[6][0][0]["employee"]), 
+                                rows8 : JSON.stringify(results[7][0][0]["employee"]), 
+                                rows9 : JSON.stringify(results[8][0][0]["employee"]), 
+                                //rows10 : JSON.stringify(results[9][0][0][""]) 
+                                // Will leave rows10 out if query10 is not needed. 
                             });
       });
 
